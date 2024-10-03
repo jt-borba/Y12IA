775f5241bd68d874bfce64046f244d736fda7c22
JT Borba <jt.borba@JTs-MacBook-Air.local>
2024-10-03 14:08:07 -0400

    moving from prev repo

---
+++ Y12IAProdSpring/src/main/java/com/example/Y12IAProd/MainController.java
@@ -0,0 +1,41 @@
+package com.example.Y12IAProd;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+
+@Controller
+public class MainController {
+    
+    @RequestMapping("/")
+    public String index() {
+        return "html/index.html"; // Redirect to static index.html
+    }
+
+    @GetMapping("/profile")
+    public String profile() {
+        return "html/profile.html"; // about.html in templates folder
+    }
+
+    @GetMapping("/contact")
+    public String contact() {
+        return "html/contact.html"; // contact.html in templates folder
+    }
+
+    @GetMapping("/nav")
+    public String nav() {
+        return "html/nav.html"; // contact.html in templates folder
+    }
+
+    @GetMapping("/pass")
+    public String pass() {
+        return "html/pass.html"; // contact.html in templates folder
+    }
+
+    @GetMapping("/reporting")
+    public String reporting() {
+        return "html/reporting.html"; // contact.html in templates folder
+    }
+
+}
